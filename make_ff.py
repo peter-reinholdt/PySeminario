671bbd4425c67b4ab0074a0c73aa03ce17037ade
Peter Reinholdt <peter.reinholdt@gmail.com>
2018-06-22 10:55:30 +0200

    Add option to only calculate certain parameters

--- make_ff.py
+++ make_ff.py
@@ -7,7 +7,8 @@ from seminario import SeminarioMethod
 parser = argparse.ArgumentParser(description='Generate force constants based on projections of the Hessian')
 parser.add_argument('-i', '--itp', dest='itp', type=str, help='Name of input .itp file', required=True)
 parser.add_argument('--fchk', dest='fchk', type=str, default='mol.fchk', help='Name of formatted checkpoint file to put into output .itp')
-parser.add_argument('--output-parms',  type=str, dest="output_parms", nargs="+", default=["bonds", "angles", "dihedrals_b", "dihedrals_k"])
+parser.add_argument('--output-parms',  type=str, dest="output_parms", nargs="+", default=["bonds", "angles", "dihedrals"], 
+                    help='Which parameters to calculate from the hessian.')
 
 args = parser.parse_args()
 
@@ -33,7 +34,7 @@ for line in itp:
         section_name  = split[1]
         print(line, end='')
     #print extracted parameters
-    elif section_name == "bonds":
+    elif section_name == "bonds" and "bonds" in args.output_parms:
         ai = int(split[0]) - 1
         aj = int(split[1]) - 1
         function_type = int(split[2])
@@ -43,7 +44,7 @@ for line in itp:
         else:
             raise NotImplementedError('Only works with harmonic bond type')
         print("{}   {}  {}  {}  {}".format(ai + 1, aj + 1, function_type, bond_length, bond_constant))
-    elif section_name == "angles":
+    elif section_name == "angles" and "angles" in args.output_parms:
         ai = int(split[0]) - 1
         aj = int(split[1]) - 1
         ak = int(split[2]) - 1
@@ -56,7 +57,7 @@ for line in itp:
         else:
             raise NotImplementedError('Only works with harmonic angle type')
         print("{}   {}  {}  {}  {}  {}".format(ai + 1, aj + 1, ak + 1, function_type, angle, angle_constant))
-    elif section_name == "dihedrals":
+    elif section_name == "dihedrals" and "dihedrals" in args.output_parms:
         ai = int(split[0]) - 1
         aj = int(split[1]) - 1
         ak = int(split[2]) - 1
@@ -66,12 +67,12 @@ for line in itp:
         if function_type == 1:
             Seminario.set_angle_unit('degree')
             dihedral = 999
-            Seminario.set_angle_unit('radian')
+            Seminario.set_angle_unit('degree')
             dihedral_constant = Seminario.get_dihedral_constant(ai, aj, ak, al)
         elif function_type == 4:
             Seminario.set_angle_unit('degree')
             dihedral = 999
-            Seminario.set_angle_unit('radian')
+            Seminario.set_angle_unit('degree')
             dihedral_constant = Seminario.get_improper_constant(ai, aj, ak, al)
         else:
             raise NotImplementedError('Only works diheral type 1 or 4')

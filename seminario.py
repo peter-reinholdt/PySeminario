33da8f3e9b4e33e064924a809a41982ef79d2c3c
Erik Kjellgren <erikkjellgren@live.dk>
2018-06-22 12:18:22 +0200

    Added modified Seminario method for angles

--- seminario.py
+++ seminario.py
@@ -58,6 +58,7 @@ class SeminarioMethod:
             if i != j:
                 self.hessian[j,i] = self._hessian_list[idx]
             j += 1
+      
             
     def set_energy_unit(self, unit):
         if unit.lower() == "kj/mol":
@@ -68,16 +69,18 @@ class SeminarioMethod:
             self._energy_unit = 1.0
         else:
             raise ValueError("Given unit not recognized")
+      
             
     def set_length_unit(self, unit):
         if unit.lower() == "nm":
             self._length_unit = 0.0529177249
         elif unit.lower() == "angstrom" or unit.lower() == "å":
             self._length_unit = 0.529177249
-        elif unit.lower() == "bohr" or unit.lower() == "au":
+        elif unit.lower() == "bohr" or unit.lower() == "au" or unit.lower() == "bohr radius":
             self._length_unit = 1.0
         else:
             raise ValueError("Given unit not recognized")
+        
             
     def set_angle_unit(self, unit):
         if unit.lower() == "degrees" or unit.lower() == "degree":
@@ -86,12 +89,15 @@ class SeminarioMethod:
             self._angle_unit = 1.0
         else:
             raise ValueError("Given unit not recognized")
+        
             
     def _calc_normal_distance_vector(self, atom_idx_A, atom_idx_B):
         return (self.coordinates[atom_idx_A,:]-self.coordinates[atom_idx_B,:])/np.linalg.norm(self.coordinates[atom_idx_A,:]-self.coordinates[atom_idx_B,:])
     
+    
     def _calc_distance_vector(self, atom_idx_A, atom_idx_B):
         return (self.coordinates[atom_idx_A,:]-self.coordinates[atom_idx_B,:])
+    
     
     def _calc_point_plane_distance(self, atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D):
         # Note that A is the point away from the plane in the calculation of the improper constant
@@ -105,7 +111,9 @@ class SeminarioMethod:
         distance = abs(a*x0 + b*y0 + c*z0 + d)/(a**2+b**2+c**2)**0.5
         return distance
     
+    
     def _calc_bond_constant(self, atom_idx_A, atom_idx_B):
+        """Equation (10) in Seminario"""
         hessian_block = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_B:3*atom_idx_B+3]
         eigvals, eigvecs = np.linalg.eig(hessian_block)
         unit_bond_vector = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
@@ -114,7 +122,9 @@ class SeminarioMethod:
             force_constant += eigvals[i]*np.abs(np.dot(unit_bond_vector, eigvecs[i]))
         return force_constant
     
+    
     def _calc_angle_constant(self, atom_idx_A, atom_idx_B, atom_idx_C):
+        """Equation (11 - 14) in Seminario"""
         hessian_block_AB = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_B:3*atom_idx_B+3]
         hessian_block_CB = -1.0*self.hessian[3*atom_idx_C:3*atom_idx_C+3,3*atom_idx_B:3*atom_idx_B+3]
         u_AB = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
@@ -139,7 +149,9 @@ class SeminarioMethod:
         force_constant = 1.0/(1.0/contribution_AB + 1.0/contribution_CB)
         return force_constant
     
+    
     def _calc_dihedral_constant(self, atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D):
+        """Equation (15 - 17) in Seminario"""
         hessian_block_AB = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_B:3*atom_idx_B+3]
         hessian_block_DC = -1.0*self.hessian[3*atom_idx_D:3*atom_idx_D+3,3*atom_idx_C:3*atom_idx_C+3]
         u_AB = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
@@ -164,7 +176,9 @@ class SeminarioMethod:
         force_constant = 1.0/(1.0/contribution_ABC + 1.0/contribution_BCD)
         return force_constant
     
+    
     def _calc_improper_constant(self, atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D):
+        """Equation (18 - 19) in Seminario"""
         hessian_block_AB = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_B:3*atom_idx_B+3]
         hessian_block_AC = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_C:3*atom_idx_C+3]
         hessian_block_AD = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_D:3*atom_idx_D+3]
@@ -181,27 +195,111 @@ class SeminarioMethod:
             k_AN += eigvals_AD[i]*np.abs(np.dot(u_N,eigvecs_AD[i]))
         force_constant = k_AN*self._calc_point_plane_distance(atom_idx_B, atom_idx_C, atom_idx_D, atom_idx_A)**2
         return force_constant
+      
+        
+    def _calc_modified_angle_constant(self, atom_idx_A, atom_idx_B, atom_idx_C, AB_angles, CB_angles):
+        """Equation (12) in Harmonic Force Constants for Molecular Mechanics Force
+        Fields via Hessian Matrix Projection. Alice E. A. Allen et. al."""
+        # This block of code is the exact same as in _calc_angle_constant()
+        # It is the original Seminario method.
+        hessian_block_AB = -1.0*self.hessian[3*atom_idx_A:3*atom_idx_A+3,3*atom_idx_B:3*atom_idx_B+3]
+        hessian_block_CB = -1.0*self.hessian[3*atom_idx_C:3*atom_idx_C+3,3*atom_idx_B:3*atom_idx_B+3]
+        u_AB = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
+        u_CB = self._calc_normal_distance_vector(atom_idx_C, atom_idx_B)
+        u_N = np.cross(u_CB, u_AB)/np.linalg.norm(np.cross(u_CB, u_AB))
+        u_PA = np.cross(u_N, u_AB)
+        u_PC = np.cross(u_N, u_CB)
+        R_AB = self._calc_distance_vector(atom_idx_A, atom_idx_B)
+        R_CB = self._calc_distance_vector(atom_idx_C, atom_idx_B)
+        eigvals_AB, eigvecs_AB = np.linalg.eig(hessian_block_AB)
+        eigvals_CB, eigvecs_CB = np.linalg.eig(hessian_block_CB)
+        contribution_AB = 0.0
+        contribution_CB = 0.0
+        for i in range(0, 3):
+            contribution_AB += eigvals_AB[i]*np.abs(np.dot(u_PA,eigvecs_AB[i]))
+            contribution_CB += eigvals_CB[i]*np.abs(np.dot(u_PC,eigvecs_CB[i]))
+        contribution_AB *= np.dot(R_AB, R_AB)
+        contribution_CB *= np.dot(R_CB, R_CB)
+        
+        # From here the correction will be calculated.
+        correction_AB = 0.0
+        if len(AB_angles) != 1:
+            print("okay AB")
+            for i in range(0, len(AB_angles)):
+                A,B,C = AB_angles[i]
+                u_AB_i = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
+                u_CB_i = self._calc_normal_distance_vector(atom_idx_C, atom_idx_B)
+                u_N_i = np.cross(u_CB_i, u_AB_i)/np.linalg.norm(np.cross(u_CB_i, u_AB_i))
+                u_PA_i = np.cross(u_N_i, u_AB_i)
+                correction_AB += np.abs(np.dot(u_PA,u_PA_i))
+            correction_AB = (correction_AB - 1.0)/(len(AB_angles) - 1.0)
+        correction_AB += 1.0
+        
+        correction_CB = 0.0
+        if len(CB_angles) != 1:
+            print("okay CB")
+            for i in range(0, len(CB_angles)):
+                C,B,A = CB_angles[i]
+                u_CB_i = self._calc_normal_distance_vector(atom_idx_C, atom_idx_B)
+                u_AB_i = self._calc_normal_distance_vector(atom_idx_A, atom_idx_B)
+                u_N_i = np.cross(u_AB_i, u_CB_i)/np.linalg.norm(np.cross(u_AB_i, u_CB_i))
+                u_PC_i = np.cross(u_CB_i, u_N_i)
+                correction_CB += np.abs(np.dot(u_PC, u_PA_i))
+            correction_CB = (correction_CB - 1.0)/(len(CB_angles) - 1.0)
+        correction_CB += 1.0
+        
+        contribution_AB = correction_AB/contribution_AB
+        contribution_CB = correction_CB/contribution_CB
+        
+        return 1.0/(contribution_AB + contribution_CB)
         
     def get_bond_constant(self, atom_idx_A, atom_idx_B):
         """Atom_idx starts counting from zero."""
         return self._calc_bond_constant(atom_idx_A, atom_idx_B)*self._energy_unit/(self._length_unit**2)
     
+    
     def get_angle_constant(self, atom_idx_A, atom_idx_B, atom_idx_C):
         """Atom_idx starts counting from zero."""
         return self._calc_angle_constant(atom_idx_A, atom_idx_B, atom_idx_C)*self._energy_unit/(self._angle_unit**2)
+    
     
     def get_dihedral_constant(self, atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D):
         """Atom_idx starts counting from zero."""
         return self._calc_dihedral_constant(atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D)*self._energy_unit/(self._angle_unit**2)
     
+    
     def get_improper_constant(self, atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D):
         """Atom_idx starts counting from zero.
         B, C, D are all connected to A. I.e. A is the central atom."""
         return self._calc_improper_constant(atom_idx_A, atom_idx_B, atom_idx_C, atom_idx_D)*self._energy_unit/(self._angle_unit**2)
+      
+        
+    def get_modified_angle_constant(self, angle_list):
+        """Takes the entire list of angles for which force
+        constants are needed. Indexing starts at 0 for atom indecies."""
+        
+        # B is the central atom in the angle
+        angle_constants = np.zeros(len(angle_list))
+        idx = 0
+        for angle in angle_list:
+            A,B,C = angle
+            AB_list = []
+            CB_list = []
+            for other_angle in angle_list:
+                if other_angle[1] == B and (other_angle[0] == A or other_angle[2] == A):
+                    AB_list.append(other_angle)
+                if other_angle[1] == B and (other_angle[0] == C or other_angle[2] == C):
+                    CB_list.append(other_angle)
+            angle_constants[idx] = self._calc_modified_angle_constant(A, B, C, AB_list, CB_list)
+            idx += 1
+                    
+        return angle_constants*self._energy_unit/(self._angle_unit**2)
+    
     
     def get_bond_length(self, atom_idx_A, atom_idx_B):
         """Atom_idx starts counting from zero."""
         return np.linalg.norm(self._calc_distance_vector(atom_idx_A, atom_idx_B)) * self._length_unit
+    
     
     def get_angle(self, atom_idx_A, atom_idx_B, atom_idx_C):
         """Atom_idx starts counting from zero."""
